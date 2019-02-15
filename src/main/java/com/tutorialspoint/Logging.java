8c80f7e43cab3513e6878358b506098226c70453
guxx_hbd888 <1360078710@qq.com>
2019-02-15 22:59:45 +0800

    AOP两种不同的实现方式基于XML配置和@Aspectj注解

---
+++ src/main/java/com/tutorialspoint/Logging.java
@@ -0,0 +1,51 @@
+package com.tutorialspoint;
+
+import org.aspectj.lang.annotation.*;
+
+/**
+ * @author hbd
+ * 定义了在各个点调用的方法
+ */
+//@AspectJ 引用一种声明方面的风格作为带有 Java 5 注释的常规 Java 类注释。
+@Aspect
+public class Logging {
+    /** Following is the definition for a pointcut to select
+     *  all the methods available. So advice will be called
+     *  for all the methods.
+     */
+    @Pointcut("execution(* com.tutorialspoint.Student.getName(..))")
+    private void selectAll(){}
+
+    /**
+     * This is the method which I would like to execute
+     * before a selected method execution.
+     */
+    @Before("selectAll()")
+    public void beforeAdvice(){
+        System.out.println("Going to setup student profile.");
+    }
+    /**
+     * This is the method which I would like to execute
+     * after a selected method execution.
+     */
+    @After("selectAll()")
+    public void afterAdvice(){
+        System.out.println("Student profile has been setup.");
+    }
+    /**
+     * This is the method which I would like to execute
+     * when any method returns.
+     */
+    @AfterReturning(pointcut = "selectAll()", returning="retVal")
+    public void afterReturningAdvice(Object retVal){
+        System.out.println("Returning:" + retVal.toString() );
+    }
+    /**
+     * This is the method which I would like to execute
+     * if there is an exception raised.
+     */
+    @AfterThrowing(pointcut = "selectAll()", throwing = "ex")
+    public void AfterThrowingAdvice(IllegalArgumentException ex){
+        System.out.println("There has been an exception: " + ex.toString());
+    }
+}
